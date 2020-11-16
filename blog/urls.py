dd658461b81136b9be6ba9bae88af4b9b60872f0
Ulukbek2424 <jenishov.ulukbek24@gmail.com>
2020-11-16 23:51:41 +0600

    work with mixins

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('', post_list, name='post_list_url'),
-    path('post/<str:slug>/', post_detail, name='post_detail_url'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('tags/', tag_list, name='tag_list_url'),
-    path('tag/<str:slug>/', tag_detail, name='tag_detail_url'),
+    path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Post, Tag
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
+
+from .models import *
+from .utils import ObjectDetailMixin
 
 
 def post_list(request):
@@ -7,9 +10,9 @@ def post_list(request):
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
 
 
 def tag_list(request):
@@ -17,6 +20,6 @@ def tag_list(request):
     return render(request, 'blog/tag_list.html', context={'tags': tags})
 
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
