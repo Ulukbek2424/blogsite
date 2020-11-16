345ca138635e120ff9c836d610765ba140148db5
Ulukbek2424 <jenishov.ulukbek24@gmail.com>
2020-11-16 22:23:23 +0600

    model Tag and includes

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from .views import *
 
 
 urlpatterns = [
-    path('', post_list, name='posts_list_url'),
+    path('', post_list, name='post_list_url'),
     path('post/<str:slug>/', post_detail, name='post_detail_url'),
+    path('tags/', tag_list, name='tag_list_url'),
+    path('tag/<str:slug>/', tag_detail, name='tag_detail_url'),
 ]
